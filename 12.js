ab243aac4dd20805973e5e5bfc038f312d004d4e
Olejjka413 <55927593+Olejjka413@users.noreply.github.com>
2021-01-30 03:58:48 +0300

    финальный

--- 12.js
+++ 12.js
@@ -8,11 +8,41 @@ var App = {
             visible: true,
             MainButton: true
         }
+    },
+    methods: {
+        len1(event) {
+            this.LastName=event.target.value
+            this.charCalc=this.LastName.length+this.FirstName.length
+            if (this.LastName!='' && this.FirstName!='') {
+                this.MainButton=false;
+            }
+            else this.MainButton=true;
+        },
+        len2(event) {
+            this.FirstName=event.target.value
+            this.charCalc=this.LastName.length+this.FirstName.length
+            if (this.LastName!='' && this.FirstName!='') {
+                this.MainButton=false;
+            }
+            else this.MainButton=true;
+        },
+        add() {
+            if (this.LastName!='' && this.FirstName!='') {
+                this.notes.push(edit(this.LastName)+" "+edit(this.FirstName))
+                this.LastName=""
+                this.FirstName=""
+                this.charCalc=0
+                this.visible=false
+                this.MainButton=true
+            }
+        }
     }
 }
 
 var app=Vue.createApp(App)
 
+
+app.mount('#app')
 
 
 function edit(str) {
